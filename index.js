f9b3a120fd8f792b96a1b70b8c82fd7f38dcae82
slytherin20 <sonaligupta2904@gmail.com>
2021-08-05 19:02:14 +0530

    Themes added

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const quantity = document.querySelector(".unit");
 const todayPrice = document.querySelector(".cost-price");
 const button = document.querySelector(".btn");
 const result = document.querySelector(".result");
-let flag = "";
+const desciptionBox= document.querySelector(".description");
+const imageBox = document.querySelector(".image-box");
+const img = document.querySelector(".img");
+const inputField = document.querySelectorAll("input");
+const container = document.querySelector(".container");
+const body = document.querySelector("body");
 
 button.addEventListener("click",profitOrLoss);
 
@@ -17,14 +22,11 @@ if(costPrice>0 && sellingPrice>0){
     let difference = sellingPrice - costPrice;
     let pOrL = (difference*qty).toFixed(2);
     if(pOrL<0){
-        flag=0;
         loss(pOrL,difference,costPrice);
     }
     else{
-        flag=1;
         profit(pOrL,difference,costPrice);
     }
-    changeTheme();
 }
 else{
     result.innerText = "Please enter a valid number."
@@ -34,15 +36,66 @@ else{
 
 function profit(profitValue,diff,CP){
     let profitPercentage = diff*100/CP;
+    changeHappyTheme();
     profitPercentage = profitPercentage.toFixed(2);
     result.innerText= `Your total profit is ₹${profitValue} which is ${profitPercentage}%`;
 }
 function loss(lossValue,diff,CP){
     diff = Math.abs(diff);
     let lossPercentage = diff*100/CP;
+    if(lossPercentage>50){
+        superSadTheme()
+    }
+    else{
+        sadTheme()
+    }
     lossValue = Math.abs(lossValue);
     lossPercentage = lossPercentage.toFixed(2);
     result.innerText= `Your total loss is ₹${lossValue} which is ${lossPercentage}%`;
 }
 
 //Change theme
+function changeHappyTheme(){
+    desciptionBox.style.backgroundColor = "green";
+    button.style.backgroundColor = "green";
+    result.style.color="green";
+    inputField[0].style.borderBottom = "solid 2px green";
+    inputField[1].style.borderBottom = "solid 2px green";
+    inputField[2].style.borderBottom = "solid 2px green";
+    body.style.backgroundColor = "white";
+    container.style.boxShadow = "5px 5px 10px 5px gainsboro";
+    
+    img.removeAttribute("src");
+    img.setAttribute("src","https://media.giphy.com/media/5fBH6zoAQg9dHK2ttsc/giphy.gif")
+    
+}
+
+function sadTheme(){
+    desciptionBox.style.backgroundColor = "brown";
+    button.style.backgroundColor = "brown";
+    result.style.color="brown";
+    inputField[0].style.borderBottom = "solid 2px brown";
+    inputField[1].style.borderBottom = "solid 2px brown";
+    inputField[2].style.borderBottom = "solid 2px brown";
+    body.style.backgroundColor = "white";
+    container.style.boxShadow = "5px 5px 10px 5px gainsboro";
+    
+    img.removeAttribute("src");
+    img.setAttribute("src","https://media.giphy.com/media/3orifdO6eKr9YBdOBq/giphy.gif")
+    
+}
+
+function superSadTheme(){
+    desciptionBox.style.backgroundColor = "#1f2021";
+    button.style.backgroundColor = "#1f2021";
+    result.style.color="#1f2021";
+    inputField[0].style.borderBottom = "solid 2px black";
+    inputField[1].style.borderBottom = "solid 2px black";
+    inputField[2].style.borderBottom = "solid 2px black";
+    body.style.backgroundColor = "black";
+    container.style.boxShadow = "none";
+    
+    img.removeAttribute("src");
+    img.setAttribute("src","https://media.giphy.com/media/ujZtlj1Y7wXyE/giphy.gif")
+    
+}
